46f8e6974c729bf13f71e219ba33da753b6ee5f8
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2020-10-21 17:58:49 -0500

    Download image using coroutines

--- CoroutinesOnAndroid/app/src/main/java/com/dartharrmi/coroutinesonandroid/MainActivity.kt
+++ CoroutinesOnAndroid/app/src/main/java/com/dartharrmi/coroutinesonandroid/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.dartharrmi.coroutinesonandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val IMAGE_URL =
+        "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutinesScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        coroutinesScope.launch {
+            val originalDeferred = coroutinesScope.async(Dispatchers.IO) {
+                getOriginalBitmap()
+            }
+            loadImage(originalDeferred.await())
+        }
+    }
+
+    private fun getOriginalBitmap() = URL(IMAGE_URL).openStream().use {
+        BitmapFactory.decodeStream(it)
+    }
+
+    private fun loadImage(bitmap: Bitmap) {
+        progressBar.visibility = GONE
+        imageView.visibility = VISIBLE
+        imageView.setImageBitmap(bitmap)
     }
 }
